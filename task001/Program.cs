41e0cc271e11eef1ad2f0e984ec7d732c3e47c47
ilmir <mr.ilmirka16@mail.ru>
2022-05-25 23:42:17 +0300

    Create reposit and task 001

---
+++ task001/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace task001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            void PrintArray(int[] array)
+            {
+                int len = array.Length;
+                for(int i = 0; i < len; i++)
+                {
+                    Console.WriteLine(array[i]);
+                }
+            }
+            Random rnd = new Random();
+            int [] array = new int[8];
+            for(int i = 0; i < array.Length; i++)
+            {
+                array[i] = rnd.Next(0,2);
+            }
+            PrintArray(array);
+        }
+    }
+}
