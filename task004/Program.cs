5f028fca607f8a24c7b6ce8e265bb3b8a65b9403
ilmir <mr.ilmirka16@mail.ru>
2022-05-26 00:07:11 +0300

    Create task004

---
+++ task004/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace task004
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            void PrintArray(int[] array)
+            {
+                int len = array.Length;
+                for(int i = 0; i < len; i++)
+                {
+                    Console.WriteLine(array[i]);
+                }
+            }
+            Random rnd = new Random();
+            int [] array = new int[123];
+            for(int i = 0; i < array.Length; i++)
+            {
+                array[i] = rnd.Next(10, 200);
+            }
+            int len = array.Length;
+            int count = 0;
+            for(int i = 0; i < len; i++)
+            {
+                if(array[i] > 10 && array[i] < 99)
+                {
+                    count = count + 1;
+                }
+            }
+            PrintArray(array);
+            Console.WriteLine($"Количество элементов из отрезка [10,99] = {count}");
+        }
+    }
+}
