10ee5769e2d9ecb8756c7de3205bf72baacec416
ilmir <mr.ilmirka16@mail.ru>
2022-05-26 00:00:42 +0300

    Create task003

---
+++ task003/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace task003
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             void PrintArray(int[] array)
+            {
+                int len = array.Length;
+                for(int i = 0; i < len; i++)
+                {
+                    Console.WriteLine(array[i]);
+                }
+            }
+            Random rnd = new Random();
+            int [] array = new int[10];
+            for(int i = 0; i < array.Length; i++)
+            {
+                array[i] = rnd.Next(100,1000);
+            }
+            int len = array.Length;
+            int posSum = 0;
+            int negSum = 0;
+            for(int i = 0; i < len; i++)
+            {
+                if(array[i] % 2 == 0)
+                {
+                    posSum = posSum + 1;
+                }
+                else
+                {
+                    negSum = negSum + 1;
+                }
+            }
+            PrintArray(array);
+            Console.WriteLine($"Количество четных чисел = {posSum}");
+            Console.WriteLine($"Количество нечетных чисел = {negSum}");
+        }
+    }
+}
